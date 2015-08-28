aeeaa934387c4a7863c96cc6e6d12d536423e5b6
thoth <thoth@athos>
2015-08-28 20:08:12 -0300

    Atualização do jquery para 1.11

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
         <title>SGI</title>
         <link rel="stylesheet" href="css/estilo.css">
         <link rel="stylesheet" href="css/bootstrap.css">
-        <script type="text/javascript" src="js/jquery-1.8.2.js"></script>
-        <script type="text/javascript" src="js/bootstrap-dropdown.js"></script>
-        <!--<script type="text/javascript" src="js/bootstrap.js"></script>-->
+        <script type="text/javascript" src="js/jquery-1.11.3.js"></script>
+        <!--<script type="text/javascript" src="js/bootstrap-dropdown.js"></script>-->
+        <script type="text/javascript" src="js/bootstrap.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
     </head>
     <body>
